27b6a4301d82bbad5a80696be137ca7adcbae38c
SutaSupapun <gugunner@gmail.com>
2026-04-02 16:51:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from datetime import date
 import json
+import streamlit as st
+st.write(type(st.secrets["FIREBASE_KEY"]))
 
 # --- Firebase Setup ---
 if not firebase_admin._apps:
-    cred_json = st.secrets["FIREBASE_KEY"]
     cred = credentials.Certificate(st.secrets["FIREBASE_KEY"])
     firebase_admin.initialize_app(cred)
 
